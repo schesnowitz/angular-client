85978a32d18c14bff1fddaa74ad8e2301924eea9
schesnowitz <steve@chesnowitz.com>
2017-04-08 18:43:16 -0400

    grabbing some data in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+import { User } from './shared/models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  users: User[];
+  constructor(private http: Http) {}
+
+  ngOnInit() {
+    this.http.get('https://reqres.in/api/users') 
+      .subscribe(data => {
+        console.warn(data.json());
+        this.users = data.json().data;
+      });
+  }
 }
